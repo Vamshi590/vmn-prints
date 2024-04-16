f96fc0285f16ec53f116016a3fd7e6837c06c499
Vamshi590 <101108609+Vamshi590@users.noreply.github.com>
2024-04-16 23:43:31 +0530

    added files

---
+++ index.js
@@ -0,0 +1,22 @@
+var name = document.getElementById("inputname").value;
+var whatsappnumber = document.getElementById("inputnumber").value;
+var city = document.getElementById("inputcity").value;
+var name = document.getElementById("inputname").value;
+
+
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', () =>{
+// //     var type = document.getElementsByName("ttype");
+// //   var vals = "";
+// //   for (var i = 0, n = type.length; i < n; i++) {
+// //     if (type[i].checked) {
+// //       vals += "," + type[i].value;
+// //     }
+// //   }
+//   if (vals) vals = vals.substring(1);
+
+//   console.log(vals);
+// })
+  
+
