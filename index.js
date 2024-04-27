6bee9cfafda06183086cb1d8453648dbecca148a
Vamshi590 <101108609+Vamshi590@users.noreply.github.com>
2024-04-28 02:50:04 +0530

    Update index.js
    updated

--- index.js
+++ index.js
@@ -4,6 +4,57 @@ var city = document.getElementById("inputcity").value;
 var name = document.getElementById("inputname").value;
 
 
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var name = document.getElementById("inputname").value;
+  var whatsappnumber = document.getElementById("inputnumber").value;
+  var city = document.getElementById("inputcity").value;
+  var numberOfTshirt = document.getElementById("numberOfTshirt").value;
+
+  var tstype = document.getElementsByName("ttype");
+  var vals = "";
+  for (var i = 0, n = tstype.length; i < n; i++) {
+    if (tstype[i].checked) {
+      vals += " ," + tstype[i].value;
+    }
+  }
+  console.log(vals);
+
+  var tsdesign = document.getElementsByName("tdesign");
+  var tval = "";
+  for (var i = 0, n = tsdesign.length; i < n; i++) {
+    if (tsdesign[i].checked) {
+      tval += " ," + tsdesign[i].value;
+    }
+    console.log(tval);
+  }
+
+  var Whatsappurl =
+    "https://wa.me/917842125042?text=" +
+    "Name of Customer : " +
+    name +
+    "%0a" +
+    "Number : " +
+    whatsappnumber +
+    "%0a" +
+    "city : " +
+    city +
+    "%0a" +
+    "Number Of Tshirts : " +
+    numberOfTshirt +
+    "%0a" +
+    "Type Of Tshirt : " +
+    vals +
+    "%0a" +
+    "Design Colour : " +
+    tval;
+
+  window.open(Whatsappurl, "_blank").focus;
+});
+
+
 // const form = document.querySelector('form');
 
 // form.addEventListener('submit', () =>{
